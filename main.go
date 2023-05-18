d857e2e3d3a8f0b7bf62eb4747ab891cc4f81967
Nobody4931 <ardenxie1@gmail.com>
2023-05-17 20:45:30 -0400

    test: create simple test game

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func printGame(game *Game, zobr *Zobrist) {
+	fmt.Printf("\nboard %X\n", zobr.Hash(game))
+	fmt.Printf("[  %2d  ]\n", game.Board[HoleCount*2+1])
+	for hole := 0; hole < HoleCount; hole++ {
+		p1Hole := holeIndex(PlayerOne, hole)
+		p2Hole := oppHoleIndex(PlayerOne, hole)
+		fmt.Printf("[%2d][%2d]\n", game.Board[p1Hole], game.Board[p2Hole])
+	}
+	fmt.Printf("[  %2d  ]\n\n", game.Board[HoleCount])
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	rand.Seed(time.Now().UnixMicro())
+
+	game := NewGameWithStones([]int{ 5, 2, 2, 5, 1, 4 }, []int{ 5, 2, 2, 5, 1, 4 })
+	zobr := NewZobrist(game)
+
+	for !game.GameOver() {
+		printGame(game, zobr)
+		recc := game.Minimax()
+		var move int
+		fmt.Printf("recc: %d\n", recc.Next.Move)
+		fmt.Printf("p%d move: ", game.Turn)
+		fmt.Scanf("%d\n", &move)
+		game.MakeMove(move)
+	}
+
+	printGame(game, zobr)
 }
